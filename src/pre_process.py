4c598b8a9fdef531e8c81d3a9a605701b5e4bedb
Richard <99752583+rxu183@users.noreply.github.com>
2023-09-23 14:28:35 -0500

    pre-processed + shuffled

--- src/pre_process.py
+++ src/pre_process.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 
 def preProcess(data_location) -> None:
+    print(os.chdir("./../dataset"))
+    print(os.listdir())
     df = pd.read_csv(data_location)
     #Shuffle input
     df = df.sample(frac=1).reset_index(drop=True)
@@ -9,5 +11,7 @@ def preProcess(data_location) -> None:
     split_index = int(df.shape[0]*0.8)
     df_train = df[0:split_index]
     df_test = df[split_index:]
-    df_train.to_csv(os.path("/dataset/cleaned_train.csv"))
-    df_test.to_csv(os.path("/dataset/cleaned_test"))
+    df_train.to_csv("cleaned_train.csv")
+    df_test.to_csv("cleaned_test.csv")
+
+preProcess("clean_1e5.csv")
